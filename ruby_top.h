8bbc541117b2ea5f3c822c088e39dc1bfe590f0c
Luis Caceres <lacaceres97@gmail.com>
2020-08-24 15:04:43 +0100

    Generate include directory as if installed

---
+++ ruby_top.h
@@ -0,0 +1,21 @@
+#ifndef RUBY_H_
+#define RUBY_H_ 1
+
+#define HAVE_RUBY_DEFINES_H     1
+#define HAVE_RUBY_DLN_H         1
+#define HAVE_RUBY_ENV_H         1
+#define HAVE_RUBY_INTERN_H      1
+#define HAVE_RUBY_MISSING_H     1
+#define HAVE_RUBY_NODE_H        1
+#define HAVE_RUBY_RE_H          1
+#define HAVE_RUBY_REGEX_H       1
+#define HAVE_RUBY_RUBY_H        1
+#define HAVE_RUBY_RUBYIO_H      1
+#define HAVE_RUBY_SIG_H         1
+#define HAVE_RUBY_ST_H          1
+#define HAVE_RUBY_UTIL_H        1
+#define HAVE_RUBY_VERSION_H     1
+
+#include "ruby/ruby.h"
+
+#endif /* RUBY_H_ */
